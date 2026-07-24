ceb0a7944861caaa81f5df1dc2fdc076701877da
태성 <sential288@gmail.com>
2026-07-24 21:20:37 +0900

    [FEAT] 공통 API 응답 및 비즈니스 예외 처리 추가

---
+++ challa-web/src/main/kotlin/com/challa/web/common/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,21 @@
+package com.challa.web.common.exception
+
+import com.challa.core.exception.BusinessException
+import com.challa.web.common.response.ApiResponse
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+    @ExceptionHandler(BusinessException::class)
+    @ResponseStatus(HttpStatus.OK)
+    fun handleBusinessException(
+        ex: BusinessException
+    ): ApiResponse<Nothing> {
+        return ApiResponse.error(
+            message = ex.message
+        )
+    }
+}
