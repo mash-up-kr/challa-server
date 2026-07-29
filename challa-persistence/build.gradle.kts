a9a507722befe2a6f759118d5a5080add82f4349
thguss <thgus345@naver.com>
2026-07-29 22:39:42 +0900

    temporary

--- challa-persistence/build.gradle.kts
+++ challa-persistence/build.gradle.kts
@@ -1,12 +1,15 @@
 plugins {
+    kotlin("jvm")
     kotlin("plugin.spring")
     kotlin("plugin.jpa")
+
+    id("org.springframework.boot")
     id("io.spring.dependency-management")
 }
 
 dependencies {
     implementation(project(":challa-core"))
-
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
     runtimeOnly("org.postgresql:postgresql")
 }
---
+++ challa-persistence/src/main/kotlin/com/challa/persistence/PersistenceConfig.kt
@@ -0,0 +1,4 @@
+package com.challa.persistence
+
+class PersistenceConfig {
+}
