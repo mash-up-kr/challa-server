d27eea5ddc82d7cf18444a2a1b85611e888dc04a
태성 <sential288@gmail.com>
2026-07-30 00:31:52 +0900

    [CHORE] JPA 엔티티 및 리포지토리 스캔 설정 추가

--- challa-persistence/src/main/kotlin/com/challa/persistence/PersistenceConfig.kt
+++ challa-persistence/src/main/kotlin/com/challa/persistence/PersistenceConfig.kt
@@ -1,4 +1,11 @@
 package com.challa.persistence
 
+import org.springframework.boot.persistence.autoconfigure.EntityScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@Configuration
+@EntityScan(basePackages = ["com.challa.persistence"])
+@EnableJpaRepositories(basePackages = ["com.challa.persistence"])
 class PersistenceConfig {
 }
