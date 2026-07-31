b880843a6c050b20cd647dff5c44d51f926dff0d
thguss <thgus345@naver.com>
2026-08-01 02:32:05 +0900

    update

--- challa-web/build.gradle.kts
+++ challa-web/build.gradle.kts
@@ -14,4 +14,5 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
