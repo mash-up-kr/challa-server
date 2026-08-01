70425c0500a6ea2a3c5d42ff07199120c319368c
thguss <thgus345@naver.com>
2026-08-01 21:18:40 +0900

    update

--- challa-core/src/main/kotlin/com/challa/core/photo/Photo.kt
+++ challa-core/src/main/kotlin/com/challa/core/photo/Photo.kt
@@ -1,7 +1,3 @@
 package com.challa.core.photo
 
-data class Photo(
-    val id: Long? = null,
-    val roomId: Long,
-    val userId: Long,
-)
+data class Photo(val id: Long? = null, val roomId: Long, val userId: Long)
