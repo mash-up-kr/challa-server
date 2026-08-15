482eb5367d0fee10571d6f7fc6c1e08495f9bae8
태성 <sential288@gmail.com>
2026-08-15 11:55:07 +0900

    [REFACTOR] 방 상세 조회 응답 id nullable 제거

--- challa-web/src/main/kotlin/com/challa/web/room/dto/GetRoomResponse.kt
+++ challa-web/src/main/kotlin/com/challa/web/room/dto/GetRoomResponse.kt
@@ -5,7 +5,7 @@ import com.challa.core.room.port.input.GetRoomResult
 import java.time.LocalDateTime
 
 data class GetRoomResponse(
-    val id: Long?,
+    val id: Long,
     val title: String,
     val totalPhotoCount: Long,
     val remainedPhotoCount: Long,
@@ -17,7 +17,7 @@ data class GetRoomResponse(
 ) {
     companion object {
         fun fromResult(result: GetRoomResult) = GetRoomResponse(
-            id = result.room.id,
+            id = result.room.id!!,
             title = result.room.title,
             totalPhotoCount = result.room.totalPhotoCount,
             remainedPhotoCount = result.room.remainedPhotoCount,
