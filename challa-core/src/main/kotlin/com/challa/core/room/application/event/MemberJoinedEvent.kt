15d7da96cb3e7d9e6ecad92b9936245c2b67a680
태성 <sential288@gmail.com>
2026-08-18 23:51:30 +0900

    [FEAT] 방 참여 WebSocket 이벤트에 유저 프로필 이미지 필드 추가

--- challa-core/src/main/kotlin/com/challa/core/room/application/event/MemberJoinedEvent.kt
+++ challa-core/src/main/kotlin/com/challa/core/room/application/event/MemberJoinedEvent.kt
@@ -2,4 +2,9 @@ package com.challa.core.room.application.event
 
 import com.challa.core.room.domain.RoomId
 
-data class MemberJoinedEvent(val roomId: RoomId, val roomTitle: String, val userNickname: String)
+data class MemberJoinedEvent(
+    val roomId: RoomId,
+    val roomTitle: String,
+    val userNickname: String,
+    val userProfileImageUrl: String?
+)
--- challa-web/src/main/kotlin/com/challa/web/event/dto/MemberJoinedResponse.kt
+++ challa-web/src/main/kotlin/com/challa/web/event/dto/MemberJoinedResponse.kt
@@ -2,12 +2,18 @@ package com.challa.web.event.dto
 
 import com.challa.core.room.application.event.MemberJoinedEvent
 
-data class MemberJoinedResponse(val id: Long, val title: String, val userNickname: String) {
+data class MemberJoinedResponse(
+    val id: Long,
+    val title: String,
+    val userNickname: String,
+    val userProfileImageUrl: String?
+) {
     companion object {
         fun fromEvent(event: MemberJoinedEvent) = MemberJoinedResponse(
             id = event.roomId,
             title = event.roomTitle,
-            userNickname = event.userNickname
+            userNickname = event.userNickname,
+            userProfileImageUrl = event.userProfileImageUrl
         )
     }
 }
